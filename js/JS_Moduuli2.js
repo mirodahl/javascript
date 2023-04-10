19bcf28b601850075564d16d8b24c8924b9f694f
mirodahl <miro.dahlgren@metropolia.fi>
2023-04-10 13:26:38 +0300

    .

---
+++ js/JS_Moduuli2.js
@@ -0,0 +1,11 @@
+//2.1 2p
+const first = prompt("Give first number");
+console.log(first);
+const second = prompt("Give second number");
+console.log(second);
+const third = prompt("Give third number");
+console.log(third);
+const fourth = prompt("Give fourth number");
+console.log(fourth);
+const fifth = prompt("Give fifth number");
+console.log(fifth);
