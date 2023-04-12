68b3c75b7f5995a6e61ac5cd52c1bf0e5a388a31
mirodahl <miro.dahlgren@metropolia.fi>
2023-04-12 14:05:14 +0300

    .

--- module3-starters-main/t3/3.js
+++ module3-starters-main/t3/3.js
@@ -1,6 +1,19 @@
 'use strict';
 //Open t3 folder in your IDE/editor. Add HTML by using innerHTML property. (2p)
 // Add the following HTML code to the element with id="target".
+//<li>John</li>
+// <li>Paul</li>
+// <li>Jones</li>
 // Add the values from 'names' array to the <li> elements in a for-loop.
 const names = ['John', 'Paul', 'Jones'];
 const targetElement = document.getElementById("target")
+
+//lisätään html-koodi
+targetElement.innerHTML = `
+    <li>John</li>
+    <li>Paul</li>
+    <li>Jones</li>  
+    `
+for (let i = 0; names.length; i++) {
+  targetElement.push({names});
+}
--- module3-starters-main/t4/4.js
+++ module3-starters-main/t4/4.js
@@ -13,3 +13,10 @@ const students = [
     id: '5423679',
   },
 ];
+
+//Open t4 folder in your IDE/editor. Add HTML by using createElement() and appenChild mehtods. (2p)
+// Add the following HTML code to the element with id="target".
+// Add the values from 'students' array to the <option> elements in a for-loop.
+// <option value="2345768">John</option>
+// <option value="2134657">Paul</option>
+// <option value="5423679">Jones</option>
