354fa4a29acaac2e32430f5c5163e1d7fdde1693
mirodahl <miro.dahlgren@metropolia.fi>
2023-04-19 15:09:12 +0300

    .

--- js/JS_Moduuli4_3.js
+++ js/JS_Moduuli4_3.js
@@ -1,19 +1,19 @@
 'use strict'
 
 //4.3
-const hakuAlku1 = "https://api.tvmaze.com/search/shows?q="
+const hakuAlku = "https://api.tvmaze.com/search/shows?q="
 const tulosAlue = document.querySelector("#results")
-const tvForm1 = document.querySelector('#tv3');
+const tvForm = document.querySelector('#tv');
 
-tvForm1.addEventListener('submit', async function (evt) {
+tvForm.addEventListener('submit', async function (evt) {
   evt.preventDefault();
-  const hakuArvo1 = document.querySelector('input[name=q]').value;
-  const hakuLause1 = hakuAlku1 + hakuArvo1
-  console.log("-- Hakulause: " + hakuLause1)
+  const hakuArvo = document.querySelector('input[name=q]').value;
+  const hakuLause = hakuAlku + hakuArvo
+  console.log("-- Hakulause: " + hakuLause)
 
   try {
     const response = await fetch(
-      hakuLause1
+      hakuLause
     );
     const jsonData = await response.json();
     console.log(jsonData);
@@ -23,7 +23,6 @@ tvForm1.addEventListener('submit', async function (evt) {
     for (let tvSarja of jsonData) {
       let h2Elem = document.createElement('h2')
       h2Elem.innerText = tvSarja.show.name
-      tulosAlue.append(articleElem)
       let imgElem = document.createElement('img')
       imgElem.src = tvSarja.show.image?.medium
       imgElem.alt = tvSarja.show.name
@@ -37,6 +36,8 @@ tvForm1.addEventListener('submit', async function (evt) {
       articleElem.appendChild(imgElem)
       articleElem.appendChild(divElem)
       articleElem.appendChild(linkElem)
+
+      tulosAlue.append(articleElem)
 
     }
 
